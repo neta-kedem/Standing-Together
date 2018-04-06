ee36d75744710b4743e902479299d442dc94be4a
yanivcogan <yanivcogan89@gmail.com>
2018-04-06 23:58:56 +0300

    Separated CSS from Core Module Code
    Moved the style object to another file, and added additional styling.

---
+++ pages/organizer/Organizer.css.js
@@ -0,0 +1,60 @@
+export default {
+	"wrapper": {
+		"display": "flex",
+		"justifyContent": "space-between",
+		"fontFamily": "Cabin, sans-serif",
+		"backgroundImage": "linear-gradient(180deg, #eaeaea, #eaeaea)"
+	},
+	"leftPanel": {
+		"position": "relative",
+		"overflow": "visible",
+		"width": 290,
+		"minHeight": 500,
+		"padding": 20,
+		"float": "left",
+		"backgroundColor": "#fbfbfb",
+		"textAlign": "center"
+	},
+	"main-panel": {
+		"display": "block",
+		"width": "100%"
+	},
+	"results-wrap": {
+		"padding": "20px"
+	},
+	"list-table":{
+		"width": "100%",
+		"borderCollapse": "collapse",
+		"fontSize":"20px"
+	},
+	"list-table-header":{
+		"padding-left": "5px",
+		"color": "rgb(128, 134, 138)",
+		"fontWeight": "700",
+		"fontSize":"18px",
+		"margin": "0 auto",
+		"width": "100%",
+		"textAlign": "left",
+	},
+	"list-table-header-field":{
+		"padding": "5px",
+		"paddingBottom": "10px"
+	},
+	"list-table-row":{
+		"backgroundColor":"#f7f5f6"
+	},
+	"list-table-field":{
+		"border":"1px solid rgb(202, 200, 199)",
+		"padding":"5px",
+		"color": "rgb(103, 111, 117)"
+	},
+	"select-all-checkbox":{
+		"color": "rgb(128, 134, 138)",
+		"fontWeight": "700"
+	},
+	"checkbox":{
+		"display": "inline-block",
+		"marginRight": "10px",
+		"color": "rgba(86, 95, 108, .7)"
+	}
+}
