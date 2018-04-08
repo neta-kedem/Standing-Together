e57a6f0820594be61c2f4220767992069bf57e64
yanivcogan <yanivcogan89@gmail.com>
2018-04-08 21:07:28 +0300

    CSS fixes
    Added minor styling changes to the <body>, through a style tag.

--- pages/Organizer.js
+++ pages/Organizer.js
@@ -30,14 +30,22 @@ render() {
 			<td style={style['list-table-field']}>{activist.phone}</td>
 			<td style={style['list-table-field']}>{activist.email}</td>
 		</tr>);
-
+	const stylesheet = `
+				body{
+					margin: 0;
+					font-family: Cabin, sans-serif;
+				}
+				`;
 	return (
-		<div>
+		<div style={style["pageWrap"]}>
 			<link
 				href='http://fonts.googleapis.com/css?family=Cabin'
 				rel='stylesheet'
 				type='text/css'
 			></link>
+			<style>
+				{stylesheet}
+			</style>
 			<TopNavBar savedViews={[{'name':'New Activists'}, {'name':'Callers in Haifa'}]}></TopNavBar>
 			<div style={style.wrapper}>
 				<div style={style.leftPanel}>
@@ -46,26 +54,28 @@ render() {
 				<div style={style['main-panel']}>
 					<QueryResultsActionMenu></QueryResultsActionMenu>
 					<div style={style['results-wrap']}>
-						<table style={style['list-table']}>
-							<thead>
-								<tr style={style['list-table-header']}>
-									<th style={style['list-table-header-field']}><FontAwesomeIcon icon="user"></FontAwesomeIcon> First Name</th>
-									<th style={style['list-table-header-field']}><FontAwesomeIcon icon="user"></FontAwesomeIcon> Last Name</th>
-									<th style={style['list-table-header-field']}><FontAwesomeIcon icon="building"></FontAwesomeIcon> Lives In</th>
-									<th style={style['list-table-header-field']}><FontAwesomeIcon icon="phone"></FontAwesomeIcon> Phone</th>
-									<th style={style['list-table-header-field']}><FontAwesomeIcon icon="envelope-open"></FontAwesomeIcon> Email</th>
-								</tr>
-							</thead>
-							<tbody>
-								{itemChilds}
-							</tbody>
-						</table>
-						<br></br>
-						<label style={style['select-all-checkbox']}>
-							<input style={{"opacity":0, "position":"absolute"}} type="checkbox"></input>
-							<div style={style['checkbox']}><FontAwesomeIcon icon="check-square"></FontAwesomeIcon></div>
-							SELECT ALL
-						</label>
+						<div style={style['query-results']}>
+							<table style={style['list-table']}>
+								<thead>
+									<tr style={style['list-table-header']}>
+										<th style={style['list-table-header-field']}><FontAwesomeIcon icon="user"></FontAwesomeIcon> First Name</th>
+										<th style={style['list-table-header-field']}><FontAwesomeIcon icon="user"></FontAwesomeIcon> Last Name</th>
+										<th style={style['list-table-header-field']}><FontAwesomeIcon icon="building"></FontAwesomeIcon> Lives In</th>
+										<th style={style['list-table-header-field']}><FontAwesomeIcon icon="phone"></FontAwesomeIcon> Phone</th>
+										<th style={style['list-table-header-field']}><FontAwesomeIcon icon="envelope-open"></FontAwesomeIcon> Email</th>
+									</tr>
+								</thead>
+								<tbody>
+									{itemChilds}
+								</tbody>
+							</table>
+							<br></br>
+							<label style={style['select-all-checkbox']}>
+								<input style={{"opacity":0, "position":"absolute"}} type="checkbox"></input>
+								<div style={style['checkbox']}><FontAwesomeIcon icon="check-square"></FontAwesomeIcon></div>
+								SELECT ALL
+							</label>
+						</div>
 					</div>
 				</div>
 			</div>
