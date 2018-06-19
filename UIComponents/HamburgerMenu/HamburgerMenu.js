1d907432232c6d2b183335b84079a139d425f975
yanivcogan <yanivcogan89@gmail.com>
2018-06-19 21:25:16 +0300

    minor naming fix

--- UIComponents/HamburgerMenu/HamburgerMenu.js
+++ UIComponents/HamburgerMenu/HamburgerMenu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class ToggleSwitch extends React.Component {
+export default class HamburgerMenu extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
