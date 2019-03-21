8767d3080cc8e2edf922876dc406c2df73248ced
yanivcogan <yanivcogan89@gmail.com>
2019-03-21 23:03:35 +0200

    mailchimp API adjustments

--- server/services/mailchimpSync.js
+++ server/services/mailchimpSync.js
@@ -1,11 +1,13 @@
 const fetch = require('node-fetch');
 const Activist = require('../models/activistModel');
 const config = require('../config');
+//the web id of the main contact list - all our contacts should appear in this list
+const mainListId = "a6f28b7b74"
 //a6f28b7b74 test list id
 const fetchMembers = function(req, res){
     let date = new Date();
     date.setMonth(date.getMonth()-1);
-    const url = 'https://us14.api.mailchimp.com/3.0/lists/a6f28b7b74/members?' +
+    const url = 'https://us14.api.mailchimp.com/3.0/lists/' + mainListId + '/members?' +
         'fields=members.email_address,members.status' +
         '&since_last_changed='+date.toISOString();
     const promise = fetch(url, {
@@ -36,11 +38,11 @@ const updateNewsletterStatus = function(contacts){
     }
     return Promise.all(updatePromises);
 };
-const createContacts = function(contacts){
+const createContacts = function(contacts, list=null){
     let updatePromises = [];
     for(let i=0; i<contacts.length; i++){
         const contact = contacts[i];
-        const url = 'https://us14.api.mailchimp.com/3.0/lists/a6f28b7b74/members/';
+        const url = 'https://us14.api.mailchimp.com/3.0/lists/' + (list ? list : mainListId) + '/members/';
         const data = {
             "email_address": contact.profile.email,
             "merge_fields": {
