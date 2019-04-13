9ac06dbdf83528516c9c3fe3d0457c83b78b06c6
yanivcogan <yanivcogan89@gmail.com>
2019-04-13 20:11:52 +0300

    cleaned unneeded functions from membership form backend

--- server/services/membershipUpdater.js
+++ server/services/membershipUpdater.js
@@ -1,50 +1,12 @@
 const mongoose = require('mongoose');
 const Activist = require('../models/activistModel');
-const ContactScan = require('../models/contactScanModel');
-const Authentication = require('../services/authentication');
 const mailchimpSync = require('../services/mailchimpSync');
 const circleFetcher = require("./circleFetcher");
 const cityFetcher = require("./cityFetcher");
 const activistsFetcher = require("./activistsFetcher");
 const arrayFunctions = require("./arrayFunctions");
 
-const markTypedContactScanRows = function(res, typerId, scanId, activists, markedDone){
-    ContactScan.findOne(
-        {"_id": scanId},
-        (err, scanData) => {
-            if (err) return res.json({success: false, error: err});
-            //iterate over new activist entries, and insert typer id and activist id details to appropriate rows
-            for(let i=0; i<activists.length; i++){
-                let activist = activists[i];
-                if(activist.metadata.scanRow) {
-                    scanData.rows[activist.metadata.scanRow].typerId = typerId;
-                    scanData.rows[activist.metadata.scanRow].activistId = activist._id;
-                }
-            }
-            //iterate over all scan rows - if all have an activist id associated with them, mark the scan as completed
-            let allRowsTyped = true;
-            for(let i=0; i<scanData.rows.length; i++){
-                if(!scanData.rows[i].activistId)
-                {
-                    allRowsTyped=false;
-                    break;
-                }
-            }
-            // if the scan went through row detection, mark it as finished IFF all rows have a corresponding typed in data.
-            // otherwise, mark it as finished IFF the typer has indicated it to be
-            scanData.complete=(scanData.rows.length&&allRowsTyped)||markedDone;
-            ContactScan.replaceOne(
-                {"_id": scanId},
-                scanData,
-                (err, result) => {
-                    if (err){
-                        return res.json(err);
-                    }
-                    return res.json({"result":"aha!", "scanData": scanData, "activists": activists});
-                }
-            );
-        });
-};
+
 const updateTypedActivists = function(activists){
     const today = new Date();
     let updatePromises = [];
@@ -70,18 +32,6 @@ const insertActivists = function(req, res){
         }
         else
             return res.json(req.body);
-    });
-};
-const toggleActivistStatus = function(req, res){
-    Authentication.hasRole(req, res, "isOrganizer").then(isUser=>{
-        if(!isUser)
-            return res.json({"error":"missing token"});
-        const activistId = req.body.activistId;
-        const isCaller = req.body.status;
-        let query = Activist.update({'_id':activistId},{'role.isCaller': isCaller});
-        return query.exec().then(()=>{
-            return res.json({"result":"set status to "+isCaller+" for user "+activistId});
-        });
     });
 };
 const addToMailchimpCircle = function(activists){
@@ -126,62 +76,47 @@ const checkForDuplicates = function (activists){
     });
     return duplicates;
 };
-const uploadTypedActivists = function (req, res){
-    Authentication.hasRole(req, res, "isTyper").then(isUser=>{
-        if(!isUser)
-            return res.json({"error":"missing token"});
-        const typerId = Authentication.getMyId();
-        const typedActivists = req.body.activists;
-        const scanId = req.body.scanId?mongoose.Types.ObjectId(req.body.scanId):null;
-        const markedDone = req.body.markedDone;
-        let newActivists = [];
-        let updatedActivists = [];
+const registerMember = function (req, res){
+        const activistData = req.body.activistData;
+        const transactionId = req.body.transactionId;
         const today = new Date();
-        for (let i=0; i<typedActivists.length; i++)
-        {
-            let curr = typedActivists[i];
-            if(!curr._id){
-                newActivists.push(
-                    {
-                        "_id": mongoose.Types.ObjectId(),
-                        "metadata" : {
-                            "creationDate" : today,
-                            "lastUpdate" : today,
-                            "joiningMethod" : "contactPage",
-                            "typerId" : typerId,
-                            "scanId": scanId,
-                            "scanRow": curr.scanRow
-                        },
-                        "profile" : {
-                            "firstName" : curr.firstName,
-                            "lastName" : curr.lastName,
-                            "phone" : curr.phone.replace(/[\-.():]/g, ''),
-                            "email" : curr.email,
-                            "residency" : curr.residency,
-                            "circle" : "תל-אביב",
-                            "isMember" : false,
-                            "isPaying" : false,
-                            "isNewsletter" : "not subscribed",
-                            "participatedEvents" : []
-                        },
-                        "role" : {
-                            "isTyper" : false,
-                            "isCaller" : false,
-                            "isOrganizer" : false,
-                            "isCircleLeader" : false
-                        },
-                        "login" : {
-                            "loginCode" : null,
-                            "token" : []
-                        }
-                    }
-                );
-            }
-            else{
-                if(!curr.locked)
-                    updatedActivists.push(curr);
-            }
-        }
+        const activistObject ={
+            "_id": mongoose.Types.ObjectId(),
+            "metadata" : {
+                "creationDate" : today,
+                "lastUpdate" : today,
+                "joiningMethod" : "webSite",
+            },
+            "profile" : {
+                "firstName" : activistData.firstName,
+                "lastName" : activistData.lastName,
+                "phone" : activistData.phone.replace(/[\-.():]/g, ''),
+                "email" : activistData.email,
+                "residency" : activistData.residency,
+                "circle" : "תל-אביב",
+                "isMember" : true,
+                "isPaying" : true
+            },
+            "membership" : {
+                "joiningDate" : today,
+                "street" : activistData.street,
+                "houseNum" : activistData.houseNum,
+                "apartmentNum" : activistData.apartmentNum,
+                "mailbox" : activistData.mailbox,
+                "TZ" : activistData.TZ,
+                "birthday" : activistData.birthday
+            },
+            "role" : {
+                "isTyper" : false,
+                "isCaller" : false,
+                "isOrganizer" : false,
+                "isCircleLeader" : false
+            },
+            "login" : {
+                "loginCode" : null,
+                "token" : []
+            },
+        };
         updateTypedActivists(updatedActivists).then(()=>{
             //mark activists whose phones or emails are already stored
             checkForDuplicates(newActivists).then(()=>{
@@ -206,7 +141,6 @@ const uploadTypedActivists = function (req, res){
                 });
             });
         });
-    });
 };
 
 module.exports = {
