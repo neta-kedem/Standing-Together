ac87380a7282c4eace99f4367f623ada7ea13802
yanivcogan <yanivcogan89@gmail.com>
2019-04-18 19:01:59 +0300

    membership registration now works
    IMPORTANT: this commit breaks the Typers page. I'll fix it later
    Israel Gives integration is WIP.

--- server/services/membershipUpdater.js
+++ server/services/membershipUpdater.js
@@ -1,91 +1,35 @@
 const mongoose = require('mongoose');
 const Activist = require('../models/activistModel');
 const mailchimpSync = require('../services/mailchimpSync');
-const circleFetcher = require("./circleFetcher");
-const cityFetcher = require("./cityFetcher");
-const activistsFetcher = require("./activistsFetcher");
-const arrayFunctions = require("./arrayFunctions");
+const circleMatcher = require("./circleMatcher");
+const activistDuplicateDetector = require("./activistDuplicateDetector");
+const israelGivesPost = require("./israelGivesPost");
 
-
-const updateTypedActivists = function(activists){
-    const today = new Date();
-    let updatePromises = [];
-    for(let i=0; i<activists.length; i++){
-        const curr = activists[i];
-        const query = Activist.updateOne({'_id':curr._id}, {
-            "profile.firstName" : curr.firstName,
-            "profile.lastName" : curr.lastName,
-            "profile.phone" : curr.phone.replace(/[\-.():]/g, ''),
-            "profile.email" : curr.email,
-            "profile.residency" : curr.residency,
-            "metadata.lastUpdate" : today,
-        });
-        updatePromises.push(query.exec());
-    }
-    return Promise.all(updatePromises);
-};
-const insertActivists = function(req, res){
-    const newActivist = new Activist(req.body);
-    newActivist.save(function (err) {
-        if (err){
-            return res.json(err);
-        }
-        else
-            return res.json(req.body);
-    });
-};
-const addToMailchimpCircle = function(activists){
+const addToCircle = function(activists){
     if(!activists || !activists.length){
         return true;
     }
-    let updatePromises = [];
-    const fetchPromises = [cityFetcher.getCities(), circleFetcher.getCircles()];
-    Promise.all(fetchPromises).then((results)=>{
-            const cities = arrayFunctions.indexByField(results[0], "name");
-            const circles = arrayFunctions.indexByField(results[1], "name");
-            for(let i = 0; i < activists.length; i++){
-                let curr = activists[i];
-                if (curr.profile.residency
-                    && cities[curr.profile.residency]
-                    && cities[curr.profile.residency].defaultCircle
-                    && circles[cities[curr.profile.residency].defaultCircle]
-                    && circles[cities[curr.profile.residency].defaultCircle].mailchimpList
-                ){
-                    updatePromises.push(mailchimpSync.createContacts([curr], circles[cities[curr.profile.residency].defaultCircle].mailchimpList));
-                }
-            }
-        }
-    );
-    return Promise.all(updatePromises);
-};
-const checkForDuplicates = function (activists){
-    const phones = activists.map((a)=>{return a.profile.phone}).filter(phone => phone && phone.length > 3);
-    const emails = activists.map((a)=>{return a.profile.email}).filter(email => email && email.length > 3);
-    const duplicates = activistsFetcher.searchDuplicates(phones, emails).then(duplicates => {
-        const duplicatesByPhone = arrayFunctions.indexByField(duplicates, "phone");
-        const duplicatesByEmail = arrayFunctions.indexByField(duplicates, "email");
+    return circleMatcher.initMatcher().then(()=>{
+        let updatePromises = [];
         for(let i = 0; i < activists.length; i++){
-            if(duplicatesByPhone[activists[i].profile.phone]){
-                activists[i].metadata.duplicateId = duplicatesByPhone[activists[i].profile.phone]._id;
-            }
-            if(duplicatesByEmail[activists[i].profile.email]){
-                activists[i].metadata.duplicateId = duplicatesByEmail[activists[i].profile.email]._id;
-            }
+            let curr = activists[i];
+            let circle = circleMatcher.getCircleByCity(curr.profile.residency);
+            curr.profile.circle = circle._id;
+            if(curr.profile.circle)
+                updatePromises.push(mailchimpSync.createContacts([curr], circle.mailchimpList));
         }
-        return activists;
+        return Promise.all(updatePromises);
     });
-    return duplicates;
 };
-const registerMember = function (req, res){
-        const activistData = req.body.activistData;
-        const transactionId = req.body.transactionId;
+const handlePayment = function
+const registerMember = function (activistData, paymentData){
         const today = new Date();
-        const activistObject ={
+        const activistObject = {
             "_id": mongoose.Types.ObjectId(),
             "metadata" : {
                 "creationDate" : today,
                 "lastUpdate" : today,
-                "joiningMethod" : "webSite",
+                "joiningMethod" : "memberPage",
             },
             "profile" : {
                 "firstName" : activistData.firstName,
@@ -93,7 +37,6 @@ const registerMember = function (req, res){
                 "phone" : activistData.phone.replace(/[\-.():]/g, ''),
                 "email" : activistData.email,
                 "residency" : activistData.residency,
-                "circle" : "תל-אביב",
                 "isMember" : true,
                 "isPaying" : true
             },
@@ -103,48 +46,83 @@ const registerMember = function (req, res){
                 "houseNum" : activistData.houseNum,
                 "apartmentNum" : activistData.apartmentNum,
                 "mailbox" : activistData.mailbox,
-                "TZ" : activistData.TZ,
-                "birthday" : activistData.birthday
+                "TZ" : activistData.tz,
+                "birthday" : activistData.birthday,
+                "transactionId" : 123
             },
             "role" : {
                 "isTyper" : false,
                 "isCaller" : false,
                 "isOrganizer" : false,
                 "isCircleLeader" : false
-            },
-            "login" : {
-                "loginCode" : null,
-                "token" : []
-            },
+            }
         };
-        updateTypedActivists(updatedActivists).then(()=>{
-            //mark activists whose phones or emails are already stored
-            checkForDuplicates(newActivists).then(()=>{
-                Activist.insertMany(newActivists).then(function (result) {
-                    if (result){
-                        let tasks = [];
-                        //create a mailchimp record in the main contact list
-                        //tasks.push(mailchimpSync.createContacts(newActivists));
-                        //create a mailchimp record in the circle-specific contact list
-                        tasks.push(addToMailchimpCircle(newActivists));
-                        //mark the activist as typed in the relevant contact scan
-                        if(scanId){
-                            tasks.push(markTypedContactScanRows(res, typerId, scanId, newActivists, markedDone));
-                        }
-                        Promise.all(tasks).then((results)=>{
-                            return res.json(results);
-                        })
-                    }
-                    else{
-                        return res.json({"error":"an unknown error has occurred, the activists were not saved"});
-                    }
-                });
+        const paymentObject = {
+            DonorTitle: "",
+            DonorFirstName: activistData.firstName,
+            DonorLastName: activistData.lastName,
+            DonorAddress1: activistData.street + " " + activistData.houseNum + ", " + activistData.apartmentNum + ", ת.ד. " + activistData.mailbox,
+            DonorCity: activistData.residency,
+            DonorCountryId: "104",
+            DonorEmail: activistData.email,
+            DonorTelephone: activistData.phone.replace(/[\-.():]/g, ''),
+            DonationSums: paymentData.selectedAmount,
+            DonationTargetTypes: "1",
+            DonationTargetIds: "580484681",
+            DonationFrequency: "2",
+            CurrencyId: "1",
+            CardTypeId: paymentData.CardTypeId,
+            CreditCardNo: paymentData.CreditCardNo,
+            CVV: paymentData.CVV,
+            ExpirationYear: paymentData.year,
+            ExpirationMonth: paymentData.month,
+            LanguageId: "1"
+        };
+        //detect activists in our system that share a phone/email with the new member
+        return activistDuplicateDetector.checkForDuplicates([activistObject]).then(()=>{
+            //if there is a circle corresponding to the member's city, add it to their details
+            //and add them to the appropriate mailchimp circle
+            return addToCircle([activistObject]).then(()=>{
+                //if the new member already exists in our system
+                if(activistObject.metadata.duplicateId){
+                    //update just the profile, membership, and lastUpdate fields - the rest might hold important information
+                    const query = Activist.updateOne({"_id": activistObject.metadata.duplicateId}, {$set: {
+                            $set: {"profile": activistObject.profile},
+                            "membership": activistObject.membership,
+                            "metadata.lastUpdate": activistObject.metadata.lastUpdate
+                        }});
+                    query.exec().then((result)=>{
+                        //pay for their membership
+                        israelGivesPost.payForMembership(paymentObject).then((result)=>{
+                            return result;
+                        }).catch((err)=>{
+                            //catch errors initiating the payment
+                            return err;
+                        });
+                    }).catch((err)=>{
+                        //catch errors updating the new member's details in our db
+                        return err;
+                    });
+                }
+                else{
+                    //if the member doesn't already appear in our db
+                    return Activist.create(activistObject).then((result)=>{
+                        //pay for their membership
+                        israelGivesPost.payForMembership(paymentObject).then((result)=>{
+                            return result;
+                        }).catch((err)=>{
+                            //catch errors initiating the payment
+                            return err;
+                        });
+                    }).catch((err)=>{
+                        //catch errors inserting the new member's details in our db
+                        return err;
+                    });
+                }
             });
         });
 };
 
 module.exports = {
-    uploadTypedActivists,
-    toggleActivistStatus,
-    insertActivists
+    registerMember
 };
