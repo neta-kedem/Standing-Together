1b432685121ac6ab8754dc5d05549d942397ac25
yanivcogan <yanivcogan89@gmail.com>
2019-04-25 02:29:59 +0300

    removed redundant event fields from creation form
    + added event category field

--- pages/EventCreation.js
+++ pages/EventCreation.js
@@ -18,11 +18,6 @@ constructor(props) {
 		title: "",
 		date: today.getDate()+"/"+(today.getMonth()+1)+"/"+today.getFullYear(),
 		category: null,
-		question1: "",
-		text1: "",
-		question2: "",
-		text2: "",
-		callScript: "",
 		categories: []
 	};
 	if(this.state["_id"]){
@@ -40,12 +35,7 @@ fetchEventDetails(){
 			this.setState({
 				title: event.eventDetails.name,
 				date: dateString,
-				category: event.eventDetails.category,
-				question1: event.callInstructions.question1,
-				text1: event.callInstructions.text1,
-				question2: event.callInstructions.question2,
-				text2: event.callInstructions.text2,
-				callScript: event.callInstructions.script,
+				category: event.eventDetails.category
 			});
 		});
 }
@@ -74,11 +64,6 @@ validateEvent() {
 		alert("please provide a date for the event");
 		return false;
 	}
-	if(!this.state.callScript.length)
-	{
-		alert("please provide a script for the event");
-		return false;
-	}
 	if(!this.state.category)
 	{
 		alert("please provide a category for the event");
@@ -98,11 +83,6 @@ handlePost() {
 			"category": this.state.category,
 		},
 		"callInstructions": {
-			"text1": this.state.text1,
-			"text2": this.state.text2,
-			"question1": this.state.question1,
-			"question2": this.state.question2,
-			"script": this.state.callScript
 		}
 	};
 	server.post('events', {'event':eventObject})
@@ -131,47 +111,23 @@ render() {
 			</TopNavBar>
 			<div dir="rtl" className="content-wrap">
 				<div className="event-details-wrap">
-					<div className="inputGroup event-identification">
-						<label className="inline-label" id="event-name">
-							<div>שם האירוע<br/>اسم الحدث</div>
-							<input type="text" name="title" value={this.state.title} onChange={this.handleInputChange.bind(this)}/>
-							</label>
-						<label className="inline-label" id="event-date">
-							<div>תאריך<br/>التاريخ</div>
-							<input dir="ltr" type="text" name="date" value={this.state.date} onChange={this.handleInputChange.bind(this)} placeholder="DD.MM.YYYY"/>
-						</label>
-						<label className="inline-label" id="event-cat">
-							<div>קטגוריה<br/>קטגוריה</div>
-							<select dir="rtl" name="category" value={this.state.category} onChange={this.handleInputChange.bind(this)}>
-								{
-									categories.map((cat)=>{
-										return <option key={"cat_" + cat._id} value={cat._id}>{cat.name.he}</option>
-									})
-								}
-							</select>
-						</label>
-					</div>
-					<label className="inline-label event-question">
-						<div>שאלה 1<br/>سؤال 1</div>
-						<textarea name="question1" value={this.state.question1} maxLength="50" onChange={this.handleInputChange.bind(this)}> </textarea>
+					<label className="label" id="event-name">
+						<div>שם האירוע<br/>اسم الحدث</div>
+						<input size="80" type="text" name="title" value={this.state.title} onChange={this.handleInputChange.bind(this)}/>
 					</label>
-					<label className="event-text">
-						<div>טקסט 1<br/>نص 1</div>
-						<textarea name="text1" value={this.state.text1} onChange={this.handleInputChange.bind(this)}> </textarea>
+					<label className="label" id="event-date">
+						<div>תאריך<br/>التاريخ</div>
+						<input size="80" dir="ltr" type="text" name="date" value={this.state.date} onChange={this.handleInputChange.bind(this)} placeholder="DD.MM.YYYY"/>
 					</label>
-					<label className="inline-label event-question">
-						<div>שאלה 2<br/>سؤال 2</div>
-						<textarea name="question2" value={this.state.question2} maxLength="50" onChange={this.handleInputChange.bind(this)}> </textarea>
-					</label>
-					<label className="event-text">
-						<div>טקסט 2<br/>نص 2</div>
-						<textarea name="text2" value={this.state.text2} onChange={this.handleInputChange.bind(this)}> </textarea>
-					</label>
-				</div>
-				<div className="event-script-wrap">
-					<label>
-						<div>תסריט שיחה<br/>سيناريو الحوار</div>
-						<textarea name="callScript" value={this.state.callScript} onChange={this.handleInputChange.bind(this)}> </textarea>
+					<label className="label" id="event-cat">
+						<div>קטגוריה<br/>קטגוריה</div>
+						<select dir="rtl" name="category" value={this.state.category} onChange={this.handleInputChange.bind(this)}>
+							{
+								categories.map((cat)=>{
+									return <option key={"cat_" + cat._id} value={cat._id}>{cat.name.he}</option>
+								})
+							}
+						</select>
 					</label>
 				</div>
 			</div>
