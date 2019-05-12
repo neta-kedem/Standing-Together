3f729caf709d13f360d7f3d081300bda39b60968
yanivcogan <yanivcogan89@gmail.com>
2019-05-13 01:19:34 +0300

    Added Token Expiration
    Currently, tokens expire if they were issued more than a week ago AND last used more than an hour ago.

--- server/services/authentication.js
+++ server/services/authentication.js
@@ -1,6 +1,11 @@
 const Activist = require('../models/activistModel');
 const MongooseUpdater = require('../services/dbHelper/mongooseUpdater');
 
+//expire tokens that have been issued this long ago:
+const TOKEN_EXPIRATION = 1000 * 60 * 60 * 24 * 7;
+//unless they were last used this long ago:
+const LAST_TOKEN_USAGE = 1000 * 60 * 60;
+
 let myId = "";
 const getUserByToken = function(req){
 	const token = req.cookies.token;
@@ -17,12 +22,36 @@ const getUserByToken = function(req){
 			return {"error":"missing token"};
 		}
 		updateLastTokenUsage(user["_id"], token);
-		myId = user["_id"];
-		return user;
+		return retireExpiredTokens(user).then((validTokens)=>{
+			for(let i = 0; i < validTokens.length; i++){
+				//if the used token hasn't expired, return the user details
+				if(validTokens[i].token === token){
+					myId = user["_id"];
+					return user;
+				}
+			}
+			//otherwise, return a missing token error
+			return {"error":"missing token"};
+		});
 	});
 	return userPromise;
 };
-
+const retireExpiredTokens = function(user){
+	let validTokens = [];
+	let now = new Date();
+	console.log(user.login);
+	for(let i = 0; i < user.login.tokens.length; i++){
+		let token = user.login.tokens[i];
+		if(token.issuedAt >= (now - TOKEN_EXPIRATION) || token.lastUsage >= (now - LAST_TOKEN_USAGE))
+		{
+			validTokens.push(token)
+		}
+	}
+	let query = Activist.updateOne({'_id': user._id}, {"login.tokens": validTokens});
+	return query.exec().then(()=>{
+		return validTokens
+	});
+};
 const updateLastTokenUsage = function(id, token){
 	const now = new Date();
 	MongooseUpdater._update(Activist,
