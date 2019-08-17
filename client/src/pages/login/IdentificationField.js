4cdf6452093edc8b06b0636cb7a9cf327f2860d0
yanivcogan <yanivcogan89@gmail.com>
2019-08-17 15:09:19 +0300

    improved login field styling

--- client/src/pages/login/IdentificationField.js
+++ client/src/pages/login/IdentificationField.js
@@ -19,29 +19,37 @@ export default class IdentificationField extends React.Component {
 			maxLength: props['maxLength'],
 			value: ''
 		};
-		this.syncStateToInput=function(event){
-			this.setState({value: event.target.value});
-		};
-		this.handlePost=function(){
-			if(this.state.validationFunction(this.state.value))
-				this.state.identificationFunction(this.state.value);
-		};
 	}
-	handleKeyPress = (event) => {
+	syncStateToInput=function(event){
+		this.setState({value: event.target.value});
+	}.bind(this);
+	handlePost=function(){
+		if(this.state.validationFunction(this.state.value))
+			this.state.identificationFunction(this.state.value);
+	}.bind(this);
+	handleKeyPress = function(event){
 		if(event.key === 'Enter'){
 			this.handlePost();
 		}
-	};
+	}.bind(this);
 	
 	render() {
 		return (
 			<div>
 				<div className='credentials-field-wrap'>	
-					<input onChange={(event)=>this.syncStateToInput(event)} onKeyPress={this.handleKeyPress} dir={this.state.dir} type={this.state.inputType} autoFocus minLength={this.state.minLength} maxLength={this.state.maxLength} className='credentials-field' placeholder={this.state.placeholder}></input>
+					<input autoFocus
+						   className='credentials-field'
+						   onChange={(event)=>this.syncStateToInput(event)}
+						   onKeyPress={this.handleKeyPress}
+						   dir={this.state.dir}
+						   type={this.state.inputType}
+						   minLength={this.state.minLength}
+						   maxLength={this.state.maxLength}
+						   placeholder={this.state.placeholder}/>
 					<div className={'login-button-wrap '+(this.state.validationFunction(this.state.value)?'valid-input ':'invalid-input ')}>
-						<div onClick={this.handlePost.bind(this)} className='login-button'>
+						<button type={"button"} onClick={this.handlePost.bind(this)} className='login-button'>
 							<FontAwesomeIcon className='login-button-icon' icon="paper-plane"/>
-						</div>
+						</button>
 					</div>
 				</div>
 			</div>
