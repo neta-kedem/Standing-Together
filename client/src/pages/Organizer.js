1ef3813cfbf13b383cd50df8fa19319616723ac2
yanivcogan <yanivcogan89@gmail.com>
2019-08-31 18:50:23 +0300

    removed leftovers in organizer page from caller campaign mechanism

--- client/src/pages/Organizer.js
+++ client/src/pages/Organizer.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import config from '../services/config';
 import server from '../services/server';
 
-import Popup from '../UIComponents/Popup/Popup'
-import Selector from '../UIComponents/Selector/Selector'
 import SelectableTable from '../UIComponents/SelectableTable/SelectableTable'
 import MultiSelect from '../UIComponents/MultiSelect/MultiSelect'
 import HamburgerMenu from '../UIComponents/HamburgerMenu/HamburgerMenu'
@@ -50,12 +47,10 @@ constructor(props) {
 			{title: ["اخر ظهور", "נראתה לאחרונה"],  visibility: false, key: "lastSeen", icon:"calendar", type:"text", width:"12em"},
 			{title: ["اخر حدث", "אירוע אחרון"],  visibility: false, key: "lastEvent", icon:"calendar-check", type:"text", width:"8em"},
 		],
-		displayEventSelectionPopup: false
 	};
 }
 
 componentDidMount() {
-	this.getPotentialEvents();
 	FilterFields.mount();
 }
 
@@ -92,13 +87,6 @@ downloadActivistsByQuery(){
 		});
 }
 
-getPotentialEvents(){
-	server.get('events/getInviteless')
-		.then(json => {
-			if(json.events)
-				this.setState({events:json});
-		});
-}
 
 handlePageNavigation(page){
 	this.setState({page: page}, ()=>{
@@ -109,19 +97,6 @@ handleFieldDisplayToggle(fieldIndex, status){
 	const tableFields = this.state.tableFields.slice();
 	tableFields[fieldIndex].visibility=status;
 	this.setState({tableFields: tableFields});
-}
-handleEventPopupToggle(){
-	this.setState({
-		displayEventSelectionPopup: !this.state.displayEventSelectionPopup,
-		campaignCreated: false
-	});
-}
-handleEventSelection(selected){
-	server.post('events/inviteByQuery', {'query':this.state.query, 'eventId':selected._id})
-		.then(json => {
-			this.setState({campaignCreated: true, selectedEventCode: json.eventCode});
-			this.getPotentialEvents();
-		});
 }
 goToActivistPage(activist){
 	this.props.history.push('/Activist?id='+activist._id);
@@ -137,36 +112,6 @@ render() {
 		selection='visibility'
 		handleChange={this.handleFieldDisplayToggle.bind(this)}/>;
 	const tableFieldsDropdown = <HamburgerMenu content={tableFieldsMultiSelect}/>;
-	const eventSelector =
-		<div>
-			<div className="event-selection-popup-title">בחירת אירוע · בחירת אירוע</div>
-			<div className="event-selector">
-				<Selector
-					options={this.state.events}
-					idIndex="__id"
-					titleIndex="name"
-					handleSelection={this.handleEventSelection.bind(this)}
-				/>
-			</div>
-			<a className="new-event-button" href="./EventCreation.js">
-				<div>
-					<div>אירוע חדש</div>
-					<div>אירוע חדש</div>
-				</div>
-			</a>
-		</div>;
-	const eventLink =
-		<div className="event-link-wrap">
-			<div className="event-link-title">
-				<div>לינק לעמוד טלפנים</div>
-				<div>לינק לעמוד טלפנים</div>
-			</div>
-			<input className="event-link" disabled value={config.serverPath+"Caller?eventCode="+this.state.selectedEventCode}/>
-			<div className="event-link-ok" onClick={this.handleEventPopupToggle.bind(this)}>
-				<div>אוקיי</div>
-				<div>אוקיי</div>
-			</div>
-		</div>;
 	return (
 		<div className="page-wrap page-wrap-organizer" dir="rtl">
 			<TopNavBar>
@@ -192,7 +137,6 @@ render() {
 				<div className="main-panel">
 					<QueryResultsActionMenu
 						items={[{"content": tableFieldsDropdown, "alignToEnd": true}]}
-						toggleEventPopup={this.handleEventPopupToggle.bind(this)}
 						activistCount={activistCount}
 						downloadActivistsByQuery={this.downloadActivistsByQuery.bind(this)}
 					/>
@@ -206,9 +150,6 @@ render() {
 						</div>
 					</div>
 				</div>
-				<Popup visibility={this.state.displayEventSelectionPopup} toggleVisibility={this.handleEventPopupToggle.bind(this)}>
-					{this.state.campaignCreated?eventLink:eventSelector}
-				</Popup>
 			</div>
 		</div>
 	)
