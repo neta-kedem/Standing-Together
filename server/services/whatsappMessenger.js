8040b67c2fa78a4ba02dac221e62cc2c3919fad1
yanivcogan <yanivcogan89@gmail.com>
2019-10-20 19:30:21 +0300

    whatsapp sender finished
    apart from translations

--- server/services/whatsappMessenger.js
+++ server/services/whatsappMessenger.js
@@ -3,14 +3,16 @@ const mongoose = require('mongoose');
 const WhatsappSession = require('../models/whatsappSessionModel');
 const Authentication = require('./authentication');
 
+const CANCEL_AFTER_NO_PING_DURATION = 1000 * 10;
+let browser = null;
 let sessionId = null;
 let currentContactIndex = 0;
 
-createSession = async (messages) => {
+createSession = async (messages, clientSessionId) => {
     const today = new Date();
     const userId = Authentication.getMyId();
     const session={
-        "_id": mongoose.Types.ObjectId(),
+        "_id": mongoose.Types.ObjectId(clientSessionId),
         "metadata":{
             "creationDate": today,
             "lastUpdate": today,
@@ -18,9 +20,8 @@ createSession = async (messages) => {
         },
         "contactCount": messages.length
     };
+    sessionId = session._id;
     return WhatsappSession.create(session).then((res, err)=>{
-        console.log(err);
-        sessionId = session._id;
         return true;
     });
 };
@@ -68,12 +69,15 @@ setProgress = async (processedCount) => {
 };
 
 closeSession = async () => {
+    if(browser){
+        browser.close();
+    }
     return WhatsappSession.deleteMany({_id: sessionId}).exec().then(()=>true);
 };
 
-sendMessage = async (messages) => {
-    const session = await createSession(messages);
-    const browser = await puppeteer.launch({headless: true});
+sendMessage = async (messages, sessionId) => {
+    const session = await createSession(messages, sessionId);
+    browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
     page.setDefaultTimeout(10000000);
     await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36');
@@ -81,6 +85,7 @@ sendMessage = async (messages) => {
     await page.waitForSelector("img[alt='Scan me!']");
     setQR(page);
     const qrInterval = setInterval(() => {
+        checkPing();
         setQR(page);
     }, 1000);
     await page.waitForSelector("img[src*='https://web.whatsapp.com/pp?']");
@@ -95,6 +100,10 @@ sendMessage = async (messages) => {
         if(!message.number || !message.number.length){
             continue;
         }
+        const isClientActive = await checkPing();
+        if(!isClientActive){
+            return;
+        }
         await page.evaluate(() => {
             window.onbeforeunload = null;
             return true;
@@ -108,13 +117,31 @@ sendMessage = async (messages) => {
         });
         await setProgress(i + 1);
     }
-    await browser.close();
     await closeSession();
     return true;
 };
 
-getProgress = async () => {
-    const query = WhatsappSession.findOne({"metadata.userId": mongoose.Types.ObjectId(Authentication.getMyId())});
+//if you ever want to implement a "pause" mechanism, do it here
+checkPing = async () => {
+    if(!sessionId)
+        return;
+    const query = WhatsappSession.findOne({_id: mongoose.Types.ObjectId(sessionId)});
+    return query.exec().then((session) => {
+        if(session && session.metadata && session.metadata.lastPing){
+            const lastPing = new Date(session.metadata.lastPing);
+            if(lastPing < new Date() - CANCEL_AFTER_NO_PING_DURATION){
+                closeSession();
+                return false;
+            }
+            //pause mechanism pseudo code:
+            //if the session is paused, set a time out to recursively call this function in 2 seconds, and return it's promise
+            return true;
+        }
+    });
+};
+
+pingSession = async (sessionId) => {
+    const query = WhatsappSession.findOneAndUpdate({_id: mongoose.Types.ObjectId(sessionId)}, {$set: {"metadata.lastPing": new Date()}});
     return query.exec().then((session) => {
         return session
     });
@@ -131,6 +158,6 @@ startSending = async (sessionId) => {
 };
 module.exports = {
     sendMessage,
-    getProgress
+    pingSession,
 };
 
