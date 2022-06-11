aada3dad6aee0c9d38c6ccf8b4a6a6a4f11d0e35
NetaKedem <neta.kedem@hibob.com>
2022-06-11 16:54:54 +0300

    extending voting system

--- server/models/voteModel.js
+++ server/models/voteModel.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const voteSchema = new mongoose.Schema({
   voterCode: String,
   votes: [String],
+  name: String,
+  tz: String,
+  phone: String,
+  email: String,
   timeStamp: { type: String, default: Date }
 });
 const voteModel = mongoose.model("vote", voteSchema);
--- server/services/voteManager.js
+++ server/services/voteManager.js
@@ -27,19 +27,34 @@ const fetchAllCandidates = async function(req, res) {
 const placeVote = async function(req, res) {
   const code = req.body.code;
   const votes = req.body.votes;
+  const name = req.body.name;
+  const tz = req.body.tz;
+  const phone = req.body.phone;
+  const email = req.body.email;
   const isCodeValidPromise = Codes.findOne({ "code": code.toUpperCase(), isUsed: { $in: [null, false] } });
   const codeDb = await isCodeValidPromise;
   if (!codeDb || codeDb.isUsed) return res.json(false);
+  if (!isIsraeliIdValid(tz)) return res.json(false);
   let updateCodePromise = Codes.updateOne(
     { _id: mongoose.Types.ObjectId(codeDb._id) },
     { isUsed: true }
   );
-  let vote = new Votes({ voterCode: code, votes: votes });
+  let vote = new Votes({ voterCode: code, votes, name, tz, phone, email });
   let updateVotePromise = vote.save();
   return Promise.all([updateCodePromise, updateVotePromise])
       .then(() => res.json(true))
-      .catch(e =>res.json(false))
+      .catch(e => res.json(false))
 };
+
+const isIsraeliIdValid = function(tz) {
+  let id = String(tz).trim()
+  if (id.length > 9 || isNaN(id) || Number(id) === 0) return false;
+  id = id.length < 9 ? ("00000000" + id).slice(-9) : id;
+  return Array.from(id, Number).reduce((counter, digit, i) => {
+    const step = digit * ((i % 2) + 1);
+    return counter + (step > 9 ? step - 9 : step);
+  }) % 10 === 0;
+}
 
 const validateCode = async function(req, res) {
   const code = req.body.code;
@@ -49,6 +64,7 @@ const validateCode = async function(req, res) {
   await delay(() => {});
   return res.json(!!isCodeValid);
 };
+
 const fetchAllVotes = async function(req, res) {
   let votes = await Votes.find({});
   return res.json(votes);
